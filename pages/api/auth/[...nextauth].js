21074e5916b4b5c7dec9635bba5109909dc97a48
jakubGrabski67 <94698014+jakubGrabski67@users.noreply.github.com>
2024-04-06 18:49:12 +0200

    changes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,22 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+const generateRandomString = (length) => {
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
+
+const generateNextAuthSecret = () => {
+  // Generate a random string of length 64
+  return generateRandomString(64);
+};
+
 export default NextAuth({
-  secret: process.env.SECRET,
+  secret: process.env.SECRET || generateNextAuthSecret(),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
